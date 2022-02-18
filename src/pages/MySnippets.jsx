573f69ce0e64449c1cd95aa7183053e399365092
Wenli Leong <Wenli.Leong@fdmgroup.com>
2022-02-18 16:13:11 +1100

    view curr user snippet

--- src/pages/MySnippets.jsx
+++ src/pages/MySnippets.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import NavigationBar from "../components/nav/NavigationBar";
 import Stack from "@mui/material/Stack";
@@ -7,18 +8,49 @@ import Grid from "@mui/material/Grid";
 import ExploreTags from "../components/ExploreTags";
 import ExploreCodes from "../components/ExploreCodes";
 
-import allCodes from "./../data/codesFile.json";
-import tags from "../data/tagsFile.json";
+// import allCodes from "./../data/codesFile.json";
+// import tags from "../data/tagsFile.json";
 
 const MySnippet = ({ cookies }) => {
   let navigate = useNavigate();
   const [selectTagList, setSelectTagList] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [allCodes, setAllCodes] = useState([]);
 
-  // TODO GET Request to get all code snippets created by user or is saved by the user
+  let config = {
+    headers: {
+      Authorization: cookies.get("Authorization"),
+    },
+  };
+
+  const getAllTags = async () => {
+    let result = await axios.get(
+      "http://localhost:8088/codeSnippetManager/code/tags",
+      config
+    );
+    if (result.status === 200) {
+      setTags(result.data);
+    }
+  };
+
+  const getUserSnippets = async () => {
+    let result = await axios.get(
+      `http://localhost:8088/codeSnippetManager/code/my/${cookies.get(
+        "UserID"
+      )}`,
+      config
+    );
+    if (result.status === 200) {
+      setAllCodes(result.data);
+    }
+  };
 
   useEffect(() => {
     if (!cookies.get("UserID")) {
       navigate("/auth");
+    } else {
+      getAllTags();
+      getUserSnippets();
     }
     console.log(selectTagList);
     // TODO GET Request to filter code snippets by selected tags
